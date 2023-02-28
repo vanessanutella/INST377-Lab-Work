17372e7d0c4a3766084dc4814f9b1368fd78e537
vanessanutella <vaguochasam@gmail.com>
2023-02-28 14:14:44 -0500

    updating lab_3_js

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -49,7 +49,7 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
- if(slidesArray === 0) {
+ if(slidePosition === 0) {
     slidePosition = totalSlides -1;
  } else {
     slidePosition -=1;
